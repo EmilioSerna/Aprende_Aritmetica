fe2c40dd43ccf738b6b00fc8786abab5c865e58a
Ángel Emilio Serna Navarro <50151743+Zelvax42@users.noreply.github.com>
2021-10-11 10:17:52 -0700

    Added TitleFragment.java and fragment_title.xml

---
+++ app/src/main/java/com/emilioserna/aprendearitmtica/TitleFragment.java
@@ -0,0 +1,23 @@
+package com.emilioserna.aprendearitmtica;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TitleFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
